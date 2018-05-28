3db16b114080c44d05724e537fae29a3e7b71159
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-05-28 21:56:54 +0200

    begin

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head profile="http://www.w3.org/2005/10/profile">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Language" content="en" />
+
+    <title>Weston Thayer's Portfolio</title>
+    
+    <meta name="description" content="My projects to date" />
+    <meta name="author" content="Weston Thayer" />
+    
+    <meta name="keywords" content="Weston Thayer, Weston, Thayer, Weston Thayer Portfolio, Product Designer, Program Manager, UX" />
+    <meta name="copyrite" content="Copyrite Weston Thayer 2011" />
+    
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/_home.css" />
+    <link rel="stylesheet" type="text/css" href="css/_fonts.css" />
+    <link rel="stylesheet" type="text/css" href="css/_grid.css" />
+
+  </head>
+
+  <body>
