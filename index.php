6552c36d7cb63b3766c3ee20f2d9a9114802778a
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-05-29 20:09:45 +0200

    design/music

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php require_once('header.php'); ?>
 <?php require_once('components/home.php'); ?>
 <?php require_once('components/store.php'); ?>
+<?php require_once('components/music.php'); ?>
 <?php require_once('components/tour-dates.php'); ?>
 <?php require_once('footer.php'); ?>
