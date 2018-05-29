8fcea988761bd0c145148d91773cab3ecb653ac6
pfadarraweb@gmail.com <pfadarraweb@gmail.com>
2018-05-29 11:37:21 +0200

    design/store

---
+++ components/store.php
@@ -0,0 +1,79 @@
+<section class="section store-home--grid">
+  <div class="store-home__head">
+    OFFICIAL STORE
+  </div>
+  <div class="store-home__products--grid">
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/t-shirt-to-the-grave" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/ftdttg.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">FTDTTG</div>
+        <div class="store-home__price">10€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/t-shirt-live" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/live.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">LIVE</div>
+        <div class="store-home__price">10€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/hoodie" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/hoddie.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">HODDIE</div>
+        <div class="store-home__price">25€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/windbreaker" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/windbreaker.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">WIND</div>
+        <div class="store-home__price">20€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/beanie" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/beannie.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">BEANNIE</div>
+        <div class="store-home__price">10€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/snapback" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/snapback.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">SNAPBACK</div>
+        <div class="store-home__price">10€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/t-shirt-titans" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/titans.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">TITANS</div>
+        <div class="store-home__price">10€</div>
+      </div>  
+    </a>
+    <a class="store-home__box--link" href="https://meltdownband.bigcartel.com/product/cd-from-this-day-to-the-grave" target="_blank">
+      <div class="store-home__box">
+        <img class="store-home__imgproduct" src="img/products/cd.png" alt=""/>
+      </div>
+      <div class="store-home__info--grid">
+        <div class="store-home__name">CD</div>
+        <div class="store-home__price">10€</div>
+      </div>  
+    </a>
+  </div>
+</section>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php require_once('header.php'); ?>
 <?php require_once('components/home.php'); ?>
+<?php require_once('components/store.php'); ?>
 <?php require_once('footer.php'); ?>
